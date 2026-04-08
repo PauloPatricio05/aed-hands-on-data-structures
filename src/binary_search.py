39dba4366a369990a8640fc09ea2525ec2679ab3
Paulo Patricio <paulocdn99@gmail.com>
2026-04-08 19:50:48 -0300

    Busca binaria

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,10 +1,17 @@
 from src.my_array import MyArray
 
-
 def binary_search(array: MyArray, target: int) -> int:
-    """
-    Realiza busca binária em um array ordenado.
+    left = 0
+    right = len(array) - 1
 
-    Deve retornar o índice do elemento ou -1 caso não encontrado.
-    """
-    raise NotImplementedError
+    while left <= right:
+        mid = (left + right) // 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
